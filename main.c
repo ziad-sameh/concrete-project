268258ae9f7a612e0a56f885facfa22f1f9d3c51
ziad_sameh <ziadsameh456@gmail.com>
2020-08-01 16:14:40 +0200

    analysis

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-float solvingtwoequations(float x,float y,float z,float w){
-return 0;
-}
+#include <math.h>
 void singleanalysis(){
- float a,b,c,d,As,Mu,fu,fy,t;
+ double a,b,c,d,As,Mu,fu,fy,t;
 printf("please enter beam depth(mm) \n");
-scanf(" %f", &t);
+scanf(" %d", &t);
 printf("please enter steel area(mm^2) \n");
-scanf(" %f", &As);
+scanf(" %d", &As);
 printf("please enter beam width(mm) \n");
-scanf(" %f", &b);
+scanf(" %d", &b);
 printf("please enter steel yield stress \n");
-scanf(" %f", &fy);
+scanf(" %d", &fy);
 printf("please enter concrete yield stress \n");
-scanf(" %f", &fu);
+scanf(" %d", &fu);
 a=(As*1.5*3*fy)/(1.15*2*b*fu);
+c=a/0.8;
 d=t-50;
-if(c/0.8>d*690/(690+fy)){
+if(c>d*690/(690+fy)){
 printf("_______________\n_______________\n\n Analysis result: \n \n");
 printf("warning:tension steel does not yield \n");
 printf("warning:section is over reinforced \n");
+double q,w,e,fs,st,p,root;
+q=2.0*b*fu/(3.0*1.5);w=As;e=-480.0*d*As;
+p=(w*w)-(4*q*e);
+if(p<0)printf("error");
+root=sqrt(p);
+printf("%d" ,root);
+st=((-w+ root)/(2*q));
+printf("%d" ,st);
+fs=600*((0.8*d/st)-1);
+printf("%d" ,fs);
+Mu=fs*As*(d-st/2);
+printf("internal moment capacity= %f \n",Mu);
 printf("_______________\n_______________\n\n \n \n");
 }
-else if(c/0.8==d*690/(690+fy)){
+else if(c==d*690/(690+fy)){
 printf("_______________\n_______________\n\n Analysis result: \n \n");
 printf("tension steel yields \n");
 printf("warning:section is over reinforced \n");
@@ -45,4 +56,4 @@ printf("_______________\n_______________\n\n \n \n");
 int main()
 {
   singleanalysis();
-}
+return 0;}
